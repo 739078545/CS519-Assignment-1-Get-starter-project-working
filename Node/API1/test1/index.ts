f31e829f01c50df6ef8f2e773490401d607f5c5b
739078545 <lizihan@bu.edu>
2024-02-29 12:12:58 -0500

    modified:   Node/API1/test1/index.ts

--- Node/API1/test1/index.ts
+++ Node/API1/test1/index.ts
@@ -2,11 +2,12 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
+    const today = new Date();
+    const dateString = today.toISOString().split('T')[0];
     const name = (req.query.name || (req.body && req.body.name));
     const responseMessage = name
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
+        : "Today's date is " + dateString; // Add this line to return today's date
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: responseMessage
