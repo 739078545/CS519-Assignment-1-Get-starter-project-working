49f5de1b41c2b71c67b8d494d0c49786ef5e164e
739078545 <739078545@qq.com>
2024-03-28 20:02:33 -0400

    modified:   Node/API1/test1/index.ts

--- Node/API1/test1/index.ts
+++ Node/API1/test1/index.ts
@@ -5,14 +5,18 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
     const today = new Date();
     const dateString = today.toISOString().split('T')[0];
     const name = (req.query.name || (req.body && req.body.name));
-    const secret2Value = process.env.Secret2 || "Default Secret Value";
-    const responseMessage = `Secret2 = ${secret2Value}`;
-    
+    const responseMessage = name
+        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+        : "Today's date is " + dateString; // Add this line to return today's date
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: responseMessage
     };
-    
+    const secretValue = process.env.Secret2 || "This is Secret #2";
+
+    // Log the desired message with the value
+    const logMessage = `Secret2 = ${secretValue}`;
+    context.log(logMessage);
 
 };
 
